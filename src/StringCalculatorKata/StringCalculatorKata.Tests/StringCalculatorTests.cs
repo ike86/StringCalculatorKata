69c1ac5fcac6352730c5d31a2f4f75f65642fdb4
Pfeff Ákos György <csopimaci@gmail.com>
2017-09-04 16:46:02 +0200

    ReturnsZero_If_NumbersIsEmpty

---
+++ src/StringCalculatorKata/StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void ReturnsZero_If_NumbersIsEmpty()
+        {
+            var calculator = new StringCalculator();
+
+            var sum = calculator.Add(string.Empty);
+
+            sum.Should().Be(0);
+        }
+    }
+}
